eed8cccd9b318e457cd16ecab498bfe461c04489
Samuel Okoro <sam@raddeviant.com>
2015-10-29 14:27:41 -0400

    Dashboard Tabs Test

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,11 @@
-var App = angular.module('vendors', []);
+var App = angular.module('vendors', ['ngRoute']);
+var dashboardTabs = [
+    {name: "Campaigns", link: "/campaigns.html", img: "img/campaings.jpg"},{name:"Collections", link:"/collections.html", img:"img/collections.jpg"},{name:"Inventory", link:"/inventory.html", img:"img/inventroy.jpg"}, {name:"Settings", link:"/settings.html", img:"img/settings.jpg"}
+]
 
 App.controller('mainCtrl', 
 function ($scope) {
       
         $scope.title = "Hello World";
-        //empty
+        $scope.dashboard = dashboardTabs;
 });
