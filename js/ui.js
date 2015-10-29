c216e1da8e8c7eac361c21f59d76e491eb0411da
Samuel Okoro <sam@raddeviant.com>
2015-10-29 15:25:52 -0400

    Dashboard Options Backgrounds

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,6 @@
 var App = angular.module('vendors', ['ngRoute']);
 var dashboardTabs = [
-    {name: "Campaigns", link: "/campaigns.html", img: "img/campaings.jpg"},{name:"Collections", link:"/collections.html", img:"img/collections.jpg"},{name:"Inventory", link:"/inventory.html", img:"img/inventroy.jpg"}, {name:"Settings", link:"/settings.html", img:"img/settings.jpg"}
+    {name: "Campaigns", link: "/campaigns.html", img: "img/campaigns.jpg"},{name:"Collections", link:"/collections.html", img:"img/collections.jpg"},{name:"Inventory", link:"/inventory.html", img:"img/inventory.jpg"}, {name:"Settings", link:"/settings.html", img:"img/settings.jpg"}
 ]
 
 App.controller('mainCtrl', 
