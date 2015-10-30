56f9f5c4a3619f06a94a05dea80c35607ff59be8
Samuel Okoro <sam@raddeviant.com>
2015-10-30 09:36:14 -0400

    Nicer Styles

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,27 @@
 var App = angular.module('vendors', ['ngRoute']);
 var dashboardTabs = [
-    {name: "Campaigns", link: "/campaigns.html", img: "img/campaigns.jpg"},{name:"Collections", link:"/collections.html", img:"img/collections.jpg"},{name:"Inventory", link:"/inventory.html", img:"img/inventory.jpg"}, {name:"Settings", link:"/settings.html", img:"img/settings.jpg"}
+    {
+        name: "Campaigns",
+        link: "/campaigns.html",
+        img: "img/campaigns.jpg"
+    }, {
+        name: "Collections",
+        link: "/collections.html",
+        img: "img/collections.jpg"
+    }, {
+        name: "Inventory",
+        link: "/inventory.html",
+        img: "img/inventory.jpg"
+    }, {
+        name: "Settings",
+        link: "/settings.html",
+        img: "img/settings.jpg"
+    }
 ]
 
-App.controller('mainCtrl', 
-function ($scope) {
-      
+App.controller('mainCtrl',
+    function ($scope) {
+
         $scope.title = "Hello World";
         $scope.dashboard = dashboardTabs;
-});
+    });
