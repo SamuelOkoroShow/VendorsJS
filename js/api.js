ffcc993af62966e5b049fdd78399193d5d047019
Samuel Okoro <sam@raddeviant.com>
2015-10-30 23:48:13 -0400

    Firebase Console Log

--- js/api.js
+++ js/api.js
@@ -1,1 +1,6 @@
-var myDataRef = new Firebase('https://resplendent-inferno-1454.firebaseio.com/');
+var myDataRef = new Firebase('https://resplendent-inferno-1454.firebaseio.com/Companies/Zappos/Campaigns/Cloths/Data');
+
+myDataRef.on("child_added", function(snapshot, prevChildKey) {
+  var newPost = snapshot.val();
+console.log(newPost);
+});
